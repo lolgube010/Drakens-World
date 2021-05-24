0fdceb0ca78cd26d255ada43a762ff90569d6268
Skellowyin <theo.benavente@elev.ga.lbs.se>
2021-05-24 15:51:07 +0200

    Sound changes
    Testing in the sound scene

---
+++ Assets/Sound/SoundScripts/Sound.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    //You can add a clip to the script from untiy...
+    public AudioClip clip;
+
+    //...and you'll be able to change volume and pitch of that clip.
+    public float volume;
+    public float pitch;
+}
