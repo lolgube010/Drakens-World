3c9e5c844c41e8f47934b3639886ff7233860ed0
LuckyLMF <79455317+LuckyLMF@users.noreply.github.com>
2021-05-24 14:42:18 +0200

    Talrik baiscly klar

---
+++ Assets/Minigame1/Scripts/plateMovment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plateMovment : MonoBehaviour
+{
+    public Rigidbody2D plate;
+    public KeyCode right;
+    public KeyCode left;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(right))
+        {
+            plate.AddForce(new Vector2(2, 0));
+        }
+        if (Input.GetKey(left))
+        {
+            plate.AddForce(new Vector2 (-2,0));
+        }
+    }
+}
