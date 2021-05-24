4caddc9bb70a862a2a32674631dc50869606fb41
Skellowyin <theo.benavente@elev.ga.lbs.se>
2021-05-24 17:09:21 +0200

    Sound changes
    Made a Semi-beta version of an AudioManager script but it wont accept audioclips for some reason

---
+++ Assets/Sound/Scripts/Sound.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//To make a custom class appear in the inspector you have to serialize it. 
+[System.Serializable]
+public class Sound : MonoBehaviour
+{
+    //Add a name to the clip.
+    public string names;
+
+    //You're able to add a clip to the script...
+    public AudioClip clip;
+
+    //Range = Adds sliders to whatever i'm putting it on.
+    //...and change it's volume and pitch.
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(.1f, 3f)]
+    public float pitch;
+
+    //HideInInspector = Like it sounds, it hides the code so it's invisible in the inspector even though it's public.
+    [HideInInspector]
+    public AudioSource source;
+}
