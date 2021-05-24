e7e516b67b5d74020715bd41e5796fde465c8497
LuckyLMF <79455317+LuckyLMF@users.noreply.github.com>
2021-05-24 15:00:22 +0200

    FoodDeath

---
+++ Assets/Minigame1/Scripts/foodDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodDeath : MonoBehaviour
+{
+    public GameObject food;
+    // detta script är för att föstöra mat objecten 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "DeathZone")
+        {
+            Destroy(food);
+        }
+    }
+}
