bcfbbe1750422ee0045b8e4e7d90f57356695673
Mohammed Osman <mohammed.osman@elev.ga.lbs.se>
2021-05-24 15:12:46 +0200

    outfit randomizer scripts
    woo!

---
+++ Assets/Scripts/Custom Karaktar/OutfitRandomizer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// mohammed
+// randomizar våra kraktärs outfits från våran outfit changer
+
+public class OutfitRandomizer : MonoBehaviour
+{
+    // skapar en lista med alla våra olika platser 
+    public List<OutfitChanger> outfitChangers = new List<OutfitChanger>();
+
+    // randomizar våra outfits
+    public void RandomizeCharacter(){
+        foreach (OutfitChanger changer in outfitChangers){
+            changer.Randomize;
+        }
+    }
+}
