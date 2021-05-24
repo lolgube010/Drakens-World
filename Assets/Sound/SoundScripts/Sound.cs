26ec6031ff8e2181bf4a9b1e8b2d284b094151ca
Skellowyin <theo.benavente@elev.ga.lbs.se>
2021-05-24 16:04:07 +0200

    Deleting the second AudioManager cuz it doesnt work

--- Assets/Sound/SoundScripts/Sound.cs
+++ Assets/Sound/SoundScripts/Sound.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//When creating a custom class you have to mark it as serializable to have it appear in the inspector.
+[System.Serializable]
 public class Sound : MonoBehaviour
 {
     //You can add a clip to the script from untiy...
