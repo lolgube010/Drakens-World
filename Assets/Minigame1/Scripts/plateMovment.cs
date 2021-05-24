799785cefe5f5f650552b10e0e3ccbe52abe806b
LuckyLMF <79455317+LuckyLMF@users.noreply.github.com>
2021-05-24 14:45:29 +0200

    Nå komenttarer

--- Assets/Minigame1/Scripts/plateMovment.cs
+++ Assets/Minigame1/Scripts/plateMovment.cs
@@ -7,22 +7,16 @@ public class plateMovment : MonoBehaviour
     public Rigidbody2D plate;
     public KeyCode right;
     public KeyCode left;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    //Väldigt baisc 2d rörelse kod -Lukas
     void Update()
     {
         if (Input.GetKey(right))
         {
-            plate.AddForce(new Vector2(2, 0));
+            plate.AddForce(new Vector2(2, 0));// för att åka höger -Lukas
         }
         if (Input.GetKey(left))
         {
-            plate.AddForce(new Vector2 (-2,0));
+            plate.AddForce(new Vector2 (-2,0)); // för att åk avänster -Lukas 
         }
     }
 }
