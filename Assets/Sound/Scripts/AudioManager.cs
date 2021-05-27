4511e701bc91cb60c0f1a4463aaed82f17cc817b
Skellowyin <theo.benavente@elev.ga.lbs.se>
2021-05-27 22:28:42 +0200

    Sound changes
    Added some mixers and made a functioning AudioManager script.

--- Assets/Sound/Scripts/AudioManager.cs
+++ Assets/Sound/Scripts/AudioManager.cs
@@ -1,34 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
+﻿using UnityEngine.Audio;
 using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    //Our other script and custom class called "Sound" makes an audioclip with volume, pitch and a name for it. Here we're making an array for that script.
     public Sound[] sounds;
 
-    // Start is called before the first frame update
+    // Start is called right when the first frame is about to run. "Awake" is like start but it's called right before the first frame.
     void Awake()
     {
+        //For each element in the array (we're nicknaming it s)...
         foreach (Sound s in sounds)
         {
+            //...we're adding an AudioSource...
+            //...and we're putting it in a variable.
             s.source = gameObject.AddComponent<AudioSource>();
-            s.source.clip = s.clip;
 
+            //Here we're basically connecting the clip, volume and pitch to the AudioSource of this element.
+            s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
         }
     }
 
-    public void Play (string names)
+    //Here we're making our own custom void called "Play" and it's gonna ask us to "Play" a string called name. So we're basically gonna name the thing we wanna "Play".
+    public void Play (string name)
     {
-        Sound s = Array.Find(sounds, sound => sound.name == name);
-        s.source.Play();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Sound/Scripts/Sound.cs
+++ Assets/Sound/Scripts/Sound.cs
@@ -1,26 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Audio;
 using UnityEngine;
 
-//To make a custom class appear in the inspector you have to serialize it. 
+//Serialization = The process of transforming data structures/objects from scripts into a format that Unity can store. To put it simply, translating data to a form that Unity "understands".
+//The MonoBehaviour class is automatically serializable, but our custom class isn't, therefore we need to make it serializable. That's why we write...
 [System.Serializable]
-public class Sound : MonoBehaviour
+
+//MonoBehaviour = The base class that all unity scripts derive from.
+//We want to create our own class called "Sound" and to do that all we have to do is erase the "MonoBehaviour" text that's beside the script name.
+//And because "MonoBehaviour" is what controlls "void Update" and "void Start" they wont be of any use to us now either.
+public class Sound 
 {
-    //Add a name to the clip.
     public string name;
 
-    //You're able to add a clip to the script...
+    //We want to reference "AudioClip" to get a audio clip and...
     public AudioClip clip;
 
-    //Range = Adds sliders to whatever i'm putting it on.
-    //...and change it's volume and pitch.
+    //...we can add volume and pitch to the clip of our choosing.
     [Range(0f, 1f)]
     public float volume;
 
+    //Range = Gives you the chance to put a minimum and maximum worth on a float.
     [Range(.1f, 3f)]
     public float pitch;
 
-    //HideInInspector = Like it sounds, it hides the code so it's invisible in the inspector even though it's public.
+    //We're making an audio source so every audioclip in the array has an audio source and is therefore able to actually produce sound lmao.
+    //HideInInspector = Hides your line of code so it doesnt show up in the Unity inspector, despite it being public.
     [HideInInspector]
     public AudioSource source;
 }
