62412b2fba3849743378fdf7d9929e166eaa3964
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-05-28 10:05:15 +0200

    Update

--- Assets/Minigame1/Scripts/foodSpawner.cs
+++ Assets/Minigame1/Scripts/foodSpawner.cs
@@ -6,12 +6,15 @@ public class foodSpawner : MonoBehaviour
 {
     public GameObject foodPrefab;
     Vector3 spawnOffset;
+    public UnityEngine.UI.Text startInfo;
+    bool hasStarted;
     private void Update()
     {
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && hasStarted != true)
         {
             spawnFood();
+            startInfo.enabled = false;
+            hasStarted = true;
         }
     }
     public void spawnFood()
