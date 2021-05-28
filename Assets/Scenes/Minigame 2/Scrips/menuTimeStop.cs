ff9b1e213373e28088e751fcf1428aafe5f79317
W0rdS0up <79453957+W0rdS0up@users.noreply.github.com>
2021-05-28 09:56:44 +0200

    start menu + fixes

---
+++ Assets/Scenes/Minigame 2/Scrips/menuTimeStop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menuTimeStop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void startTime()
+    {
+        Time.timeScale = 1;
+    }
+}
