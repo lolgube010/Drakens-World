e9601bc9d2f8a1b998248789e10d1b58d0f0acfa
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-05-28 09:48:10 +0200

    FoodSpawnern

---
+++ Assets/Minigame1/Scripts/foodSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class foodSpawner : MonoBehaviour
+{
+    public GameObject foodPrefab;
+    Vector3 spawnOffset;
+    private void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            spawnFood();
+        }
+    }
+    public void spawnFood()
+    {
+        spawnOffset = gameObject.transform.position;
+        spawnOffset.x += Random.Range(2f, -3f);
+        Instantiate(foodPrefab, spawnOffset, Quaternion.identity);
+    }
+}
