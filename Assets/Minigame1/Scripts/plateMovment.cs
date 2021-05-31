4c6fab305d5890e6e9fd1119ee9b42b814699319
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-05-31 13:40:15 +0200

    food no workey

--- Assets/Minigame1/Scripts/plateMovment.cs
+++ Assets/Minigame1/Scripts/plateMovment.cs
@@ -12,67 +12,13 @@ public class plateMovment : MonoBehaviour
     
     void Update()
     {
-        StartCoroutine("Accelerate");
         if (Input.GetKey(right))
         {
-            
-            //plate.AddForce(new Vector2(2, 0));// för att åka höger -Lukas
+            plate.AddForce(new Vector2(5, 0));// för att åka höger -Lukas
         }
         if (Input.GetKey(left))
         {
-            //plate.AddForce(new Vector2 (-2,0)); // för att åk avänster -Lukas 
-        }
-    }
-    public float speed;
-    Rigidbody2D rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    void FixedUpdate()
-    {
-        rb.velocity = Vector2.right * speed;
-    }
-
-    IEnumerator Accelerate()
-    {
-        if (Input.GetKeyDown(right))
-        {
-            for (float i = speed; i < 4; i += 0.5f)
-            {
-                speed = i;
-                yield return new WaitForSeconds(.1f);
-            }
-        }
-        else if (Input.GetKeyDown(left))
-        {
-            for (float i = speed; i > -4; i -= 0.5f)
-            {
-                speed = i;
-                yield return new WaitForSeconds(.1f);
-            }
-        }
-        else
-        {
-            if (speed >= 0)
-            {
-                for (float i = speed; i == 0; i -= 0.5f)
-                    {
-                        speed = i;
-                        yield return new WaitForSeconds(.1f);
-                    }
-            }
-            else
-            {
-                for (float i = speed; i == 0; i -= 0.5f)
-                {
-                    speed = i;
-                    yield return new WaitForSeconds(.1f);
-                }
-            }
-
+            plate.AddForce(new Vector2 (-5,0)); // för att åk avänster -Lukas 
         }
     }
 }
