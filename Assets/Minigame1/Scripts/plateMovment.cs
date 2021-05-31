a137d805f2be51353308bd99075ec956f67c7d96
Christian-Baza <79457485+Christian-Baza@users.noreply.github.com>
2021-05-31 13:27:32 +0200

    aaaa

--- Assets/Minigame1/Scripts/plateMovment.cs
+++ Assets/Minigame1/Scripts/plateMovment.cs
@@ -9,19 +9,21 @@ public class plateMovment : MonoBehaviour
     public KeyCode right;
     public KeyCode left;
     //Väldigt baisc 2d rörelse kod -Lukas 
-    /*
+    
     void Update()
     {
+        StartCoroutine("Accelerate");
         if (Input.GetKey(right))
         {
-            plate.AddForce(new Vector2(2, 0));// för att åka höger -Lukas
+            
+            //plate.AddForce(new Vector2(2, 0));// för att åka höger -Lukas
         }
         if (Input.GetKey(left))
         {
-            plate.AddForce(new Vector2 (-2,0)); // för att åk avänster -Lukas 
+            //plate.AddForce(new Vector2 (-2,0)); // för att åk avänster -Lukas 
         }
-    }*/
-    public float speed = 10f;
+    }
+    public float speed;
     Rigidbody2D rb;
 
     void Start()
@@ -32,5 +34,45 @@ public class plateMovment : MonoBehaviour
     void FixedUpdate()
     {
         rb.velocity = Vector2.right * speed;
+    }
+
+    IEnumerator Accelerate()
+    {
+        if (Input.GetKeyDown(right))
+        {
+            for (float i = speed; i < 4; i += 0.5f)
+            {
+                speed = i;
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+        else if (Input.GetKeyDown(left))
+        {
+            for (float i = speed; i > -4; i -= 0.5f)
+            {
+                speed = i;
+                yield return new WaitForSeconds(.1f);
+            }
+        }
+        else
+        {
+            if (speed >= 0)
+            {
+                for (float i = speed; i == 0; i -= 0.5f)
+                    {
+                        speed = i;
+                        yield return new WaitForSeconds(.1f);
+                    }
+            }
+            else
+            {
+                for (float i = speed; i == 0; i -= 0.5f)
+                {
+                    speed = i;
+                    yield return new WaitForSeconds(.1f);
+                }
+            }
+
+        }
     }
 }
